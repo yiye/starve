2dd42c614b9a832c4e296fd9b9e396425dc78720
何道 <shuangling.ysl@alibaba-inc.com>
2017-01-10 12:53:09 +0800

    my world

---
+++ Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,15 @@
+return {
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  } 
+}
